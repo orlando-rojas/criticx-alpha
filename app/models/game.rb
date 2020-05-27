b28d5cb3d79406bad5695b5961a9b7c7a735a673
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-05-27 16:13:18 -0500

    polymorphic

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,5 @@ class Game < ApplicationRecord
   has_and_belongs_to_many :platforms, :genres
   has_many :expansions, class_name: 'Game', foreign_key: :parent_id
   belongs_to :parent, class_name: 'Game', optional: true
+  has_many :reviews, as: :reviewable
 end
