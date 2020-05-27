ada81fa10ab906355ae6da391705dc9d720b6ec7
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-05-27 17:04:31 -0500

    add json data

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,11 +14,16 @@ class ParentGameValidator < ActiveModel::EachValidator
 end
 
 class Game < ApplicationRecord
-  has_and_belongs_to_many :platforms, :genres
+  has_many :involved_companies
+  has_many :companies, through: :involved_companies
   has_many :expansions, class_name: 'Game', foreign_key: :parent_id
-  belongs_to :parent, class_name: 'Game', optional: true
   has_many :reviews, as: :reviewable
+  has_and_belongs_to_many :platforms
+  has_and_belongs_to_many :genres
+  belongs_to :parent, class_name: 'Game', optional: true
+
   enum category: { main_game: 0, expansion: 1 }
+
   validates :name, presence: true, uniqueness: true
   validates :category, presence: true
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,96 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_genres_relationships(new_game, genres)
+  begin
+    genres.each { |genre| new_game.genres << Genre.find_by(name: genre) }
+  rescue
+    p "Genre relationship not created for #{new_game}"
+  end
+end
+
+def create_platforms_relationships(new_game, platforms)
+  return if platforms.nil?
+
+  begin
+    platforms.each { |platform| new_game.platforms << Platform.find_by(name: platform['name']) }
+  rescue
+    p "Platform relationship not created for #{new_game}"
+  end
+end
+
+def create_involved_companies_relationships(new_game, involved_companies)
+  return if involved_companies.nil?
+
+  begin
+    involved_companies.each do |involved_company|
+      company = Company.find_by(name: involved_company['name'])
+      InvolvedCompany.create(company: company,
+                             game: new_game,
+                             developer: involved_company['developer'],
+                             publisher: involved_company['publisher'])
+    end
+  rescue
+    p "Involved company relationship not created for #{new_game}"
+  end
+end
+
+def create_game_relationships(new_game, game)
+  create_genres_relationships(new_game, game['genres'])
+  create_platforms_relationships(new_game, game['platforms'])
+  create_involved_companies_relationships(new_game, game['involved_companies'])
+end
+
+puts 'Start seeding data'
+
+companies = JSON.parse(File.read('db/companies.json'))
+platforms = JSON.parse(File.read('db/platforms.json'))
+genres = JSON.parse(File.read('db/genres.json'))
+games = JSON.parse(File.read('db/games.json'))
+
+puts 'Loading companies'
+companies.each do |company|
+  new_company = Company.new(company)
+  p "#{new_company} not created" unless new_company.save
+end
+
+puts 'Loading platforms'
+platforms.each do |platform|
+  new_platform = Platform.new(platform)
+  p "#{new_platform} not created" unless new_platform.save
+end
+
+puts 'Loading genres'
+genres['genres'].each do |genre|
+  new_genre = Genre.new(name: genre)
+  p "#{new_genre} not created" unless new_genre.save
+end
+
+puts 'Loading main games and relations'
+
+main_games = games.select { |game| game['parent'].nil? }
+
+main_games.each do |game|
+  new_game = Game.new(game.slice('name', 'summary', 'release_date', 'category', 'rating'))
+  if new_game.save
+    create_game_relationships(new_game, game)
+  else
+    puts "Main game: #{new_game} not created"
+  end
+end
+
+puts 'Loading expansion games and relations'
+
+expansion_games = games.reject { |game| game['parent'].nil? }
+
+expansion_games.each do |game|
+  game_data = game.slice('name', 'summary', 'release_date', 'category', 'rating')
+  game_data['parent'] = Game.find_by(name: game['parent'])
+  new_game = Game.new(game_data)
+
+  if new_game.save
+    create_game_relationships(new_game, game)
+  else
+    puts "Expansion: #{new_game} not created"
+    puts "Reason: #{new_game.errors.to_a}"
+  end
+end
+
+puts 'End seeding data'
